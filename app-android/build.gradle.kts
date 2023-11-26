8e5c4d4552fd2bed12f76415e69107a6699405f2
Taush Sampley <taushsampley@gmail.com>
2023-11-25 20:47:26 -0600

    Fix generated import

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:entities"))
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
