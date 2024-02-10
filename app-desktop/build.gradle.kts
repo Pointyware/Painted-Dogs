c5ac3138e7099d861282b55ca4eb88031fc5834a
Taush Sampley <taushsampley@gmail.com>
2024-02-10 16:23:04 -0600

    Setup desktop entry point

--- app-desktop/build.gradle.kts
+++ app-desktop/build.gradle.kts
@@ -4,6 +4,19 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation(compose.ui)
+    implementation(compose.preview)
+    implementation(compose.material3)
+}
+
+compose.desktop {
+    application {
+        mainClass = "org.pointyware.painteddogs.MainKt"
+    }
 }
--- app-desktop/src/main/java/org/pointyware/painteddogs/Main.kt
+++ app-desktop/src/main/java/org/pointyware/painteddogs/Main.kt
@@ -1,4 +1,20 @@
 package org.pointyware.painteddogs
 
-class Main {
+import androidx.compose.material3.Text
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+
+fun main() = application {
+
+    val state = rememberWindowState()
+    Window(
+        title = "My Application",
+        state = state,
+        onCloseRequest = this::exitApplication
+    ) {
+        Text(
+            text = "Hello, App!"
+        )
+    }
 }
