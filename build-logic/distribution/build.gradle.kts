fabe269cbc23ac040899dc92df561fd80114ae56
Taush Sampley <taushsampley@gmail.com>
2024-02-11 20:58:49 -0600

    Add distribution module to build-logic

---
+++ build-logic/distribution/build.gradle.kts
@@ -0,0 +1,14 @@
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "org.pointyware.painteddogs.buildlogic.distribution"
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation("com.google.apis:google-api-services-androidpublisher:v3-rev20240207-2.0.0")
+}
