ba8d26f8db8df6ac61fa2d0773f2d92f54c30c59
Taush Sampley <taushsampley@gmail.com>
2024-02-18 10:46:43 -0600

    create play account data

---
+++ build-logic/distribution/src/main/kotlin/org/pointyware/painteddogs/buildlogic/distribution/google/PlayAccount.kt
@@ -0,0 +1,9 @@
+package org.pointyware.painteddogs.buildlogic.distribution.google
+
+/**
+ * TODO: describe purpose/intent of PlayAccount
+ */
+data class PlayAccount(
+    val email: String,
+    val keyFile: String
+)
