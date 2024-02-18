a4fbbb0554fb3636c666ddd1fd8fcdb5e2c20823
Taush Sampley <taushsampley@gmail.com>
2024-02-18 10:00:37 -0600

    revise interface; cleanup reference docs

--- build-logic/distribution/src/main/kotlin/org/pointyware/painteddogs/buildlogic/distribution/GoogleDistribution.kt
+++ build-logic/distribution/src/main/kotlin/org/pointyware/painteddogs/buildlogic/distribution/GoogleDistribution.kt
@@ -1,15 +1,11 @@
 package org.pointyware.painteddogs.buildlogic.distribution
 
+import kotlinx.coroutines.flow.Flow
 import java.io.File
 
 
 /**
  * Defines a transactional interface to interact with the Google Play Publishing API
- *
- * https://googleapis.dev/java/google-api-services-androidpublisher/latest/com/google/api/services/androidpublisher/AndroidPublisher.html
- *
- * https://googleapis.dev/java/google-api-services-androidpublisher/latest/com/google/api/services/androidpublisher/AndroidPublisher.Builder.html
- *
  */
 interface GoogleDistribution {
 
@@ -19,57 +15,19 @@ interface GoogleDistribution {
     interface Edit {
         /**
          * The AAB file to upload
-         *
-         *       # Call Edits.bundles:Upload
-         *       curl -X POST \
-         *         -H "X-Goog-User-Project: $PROJECT_ID" \
-         *         -H "Authorization: Bearer $TOKEN" \
-         *         https://androidpublisher.googleapis.com/upload/androidpublisher/v3/applications/$BUNDLE_ID/edits:insert
          */
         var bundle: File?
-
-        /**
-         *
-         *       # Call Edits.tracks:Update
-         *       curl -X POST \
-         *         -H "X-Goog-User-Project: $PROJECT_ID" \
-         *         -H "Authorization: Bearer $TOKEN" \
-         *         https://androidpublisher.googleapis.com/upload/androidpublisher/v3/applications/$BUNDLE_ID/edits:insert
-         */
-        fun updateTracks()
-
-        /**
-         *
-         *       # Call Edits.listings:Update/Patch
-         *       curl -X POST \
-         *         -H "X-Goog-User-Project: $PROJECT_ID" \
-         *         -H "Authorization: Bearer $TOKEN" \
-         *         https://androidpublisher.googleapis.com/upload/androidpublisher/v3/applications/$BUNDLE_ID/edits:insert
-         */
-        fun updateListing()
-
-        /**
-         * Commits the changes to the Google Play Store. The edit is no longer valid after this call.
-         *
-         *       # Call Edits:Commit
-         *
-         *       curl -X POST \
-         *         -H "X-Goog-User-Project: $PROJECT_ID" \
-         *         -H "Authorization: Bearer $TOKEN" \
-         *         https://androidpublisher.googleapis.com/upload/androidpublisher/v3/applications/$BUNDLE_ID/edits/{editId}/bundles
-         */
-        fun commit()
+        fun updateTracks(tracks: List<Track>)
+        fun updateListing(details: ListingsDetails)
+        fun executeUpdate(): Flow<Result<Progress>>
     }
-    /**
-     * Begin a new edit
-     *
-     * https://googleapis.dev/java/google-api-services-androidpublisher/latest/com/google/api/services/androidpublisher/AndroidPublisher.Edits.Insert.html
-     *
-     *       # Call Edits:Insert
-     *       curl -X POST \
-     *         -H "X-Goog-User-Project: $PROJECT_ID" \
-     *         -H "Authorization: Bearer $TOKEN" \
-     *         https://androidpublisher.googleapis.com/upload/androidpublisher/v3/applications/$BUNDLE_ID/edits:insert
-     */
     fun createEdit(): Edit
+
+    sealed class Progress {
+        data class InProgress(val progress: Float): Progress()
+        data class Complete(val editId: String): Progress()
+    }
+
+    data class Track(val name: String, val userFraction: Double = 1.0, val versionCodes: List<Long>)
+    data class ListingsDetails(val extra: String)
 }
