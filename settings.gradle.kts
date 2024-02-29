c4c878a940924746f56f46549e5351ad13be6d82
Taush Sampley <taushsampley@gmail.com>
2024-02-29 02:05:20 -0600

    Add feature modules with starter files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,16 @@ dependencyResolutionManagement {
 
 rootProject.name = "painted-dogs"
 include(":core:entities")
+include(":feature:chat")
+include(":feature:collections")
+include(":feature:collections:core")
+include(":feature:collections:crowdfunding")
+include(":feature:events")
+include(":feature:groups")
 include(":feature:login")
+include(":feature:payments")
+include(":feature:payments:core")
+include(":feature:profiles")
 include(":app-android")
 //include(":app-ios")
 include(":app-desktop")
