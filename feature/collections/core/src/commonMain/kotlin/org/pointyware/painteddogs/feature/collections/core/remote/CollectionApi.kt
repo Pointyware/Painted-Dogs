09027257f72f020f2735f7e344f8d6b10f319f4d
Taush Sampley <taushsampley@gmail.com>
2024-03-03 21:09:23 -0600

    Create test collection api implementation

--- feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/remote/CollectionApi.kt
+++ feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/remote/CollectionApi.kt
@@ -1,5 +1,25 @@
 package org.pointyware.painteddogs.feature.collections.core.remote
 
-class CollectionApi {
+import org.pointyware.painteddogs.core.entities.CurrencyAmount
+import org.pointyware.painteddogs.core.entities.Uuid
+import org.pointyware.painteddogs.feature.collections.core.Collection
+import org.pointyware.painteddogs.feature.collections.core.crowdfundingCollection
 
+interface CollectionApi {
+    fun startDonationDrive(
+        title: String,
+        description: String,
+        targetAmount: CurrencyAmount
+    ): Result<Collection>
+}
+
+class TestCollectionApi : CollectionApi {
+    override fun startDonationDrive(
+        title: String,
+        description: String,
+        targetAmount: CurrencyAmount
+    ): Result<Collection> {
+        val newId = Uuid.v4()
+        return Result.success(crowdfundingCollection(newId, title, description, targetAmount))
+    }
 }
