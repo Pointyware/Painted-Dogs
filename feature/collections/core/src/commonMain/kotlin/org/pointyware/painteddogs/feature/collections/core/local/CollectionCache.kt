685782da22b7c5b610f24e711530179b60d4c0c5
Taush Sampley <taushsampley@gmail.com>
2024-03-03 20:54:40 -0600

    Implement CollectionCache as naive in-memory map cache

--- feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/local/CollectionCache.kt
+++ feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/local/CollectionCache.kt
@@ -1,5 +1,19 @@
 package org.pointyware.painteddogs.feature.collections.core.local
 
-class CollectionCache {
+import org.pointyware.painteddogs.core.entities.Uuid
+import org.pointyware.painteddogs.feature.collections.core.Collection
 
+interface CollectionCache {
+    fun save(it: Collection)
+}
+
+class InMemoryCollectionCache : CollectionCache {
+
+    /**
+     * Mutable map of collections
+     */
+    private var collections: MutableMap<Uuid, Collection> = mutableMapOf()
+    override fun save(it: Collection) {
+        collections[it.id] = it
+    }
 }
