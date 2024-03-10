f0ce2579231390973dbe1ca44b3cd55b99efee48
Taush Sampley <taushsampley@gmail.com>
2024-03-10 13:33:02 -0500

    Configure source links

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+import java.net.URL
+
 plugins {
     //trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidApplication).apply(false)
@@ -11,4 +14,16 @@ plugins {
 
 subprojects {
     apply(plugin = "org.jetbrains.dokka")
+
+    tasks.withType<DokkaTask>().configureEach {
+
+        dokkaSourceSets.configureEach {
+
+            sourceLink {
+                localDirectory.set(projectDir.resolve("src"))
+                remoteUrl.set(URL("https://github.com/Pointyware/Painted-Dogs/tree/main/src"))
+                remoteLineSuffix.set("#L")
+            }
+        }
+    }
 }
