951d335b26ea6d1fe19188fe683db0a9a6d23d9d
Taush Sampley <taushsampley@gmail.com>
2024-03-11 00:12:59 -0500

    Comment unused values

--- feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/CollectionType.kt
+++ feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/CollectionType.kt
@@ -2,9 +2,9 @@ package org.pointyware.painteddogs.feature.collections.core
 
 enum class CollectionType {
     CROWDFUNDING,
-    FUNDRAISING,
-    MEMBERSHIP,
-    SUBSCRIPTION,
-    PROCEEDS,
-    OTHER
+//    FUNDRAISING,
+//    MEMBERSHIP,
+//    SUBSCRIPTION,
+//    PROCEEDS,
+//    OTHER
 }
