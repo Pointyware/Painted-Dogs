56fdf3ef3a49e7eb69b44b5559bd69d75781b8e8
Taush Sampley <taushsampley@gmail.com>
2024-03-11 00:07:51 -0500

    Remove redundant type; add predicted types

--- feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/CollectionType.kt
+++ feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/CollectionType.kt
@@ -1,6 +1,10 @@
 package org.pointyware.painteddogs.feature.collections.core
 
 enum class CollectionType {
-    DONATION,
-    CROWDFUNDING
+    CROWDFUNDING,
+    FUNDRAISING,
+    MEMBERSHIP,
+    SUBSCRIPTION,
+    PROCEEDS,
+    OTHER
 }
