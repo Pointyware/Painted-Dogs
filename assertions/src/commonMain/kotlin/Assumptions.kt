b2acf0d6ff6482ee3b341d3495fd0a0246a906a3
Taush Sampley <taushsampley@gmail.com>
2024-03-19 20:40:29 -0500

    Move TestAssumptions to assumptions file

--- assertions/src/commonMain/kotlin/Assumptions.kt
+++ assertions/src/commonMain/kotlin/Assumptions.kt
@@ -7,6 +7,23 @@ interface Assumptions {
     fun that(subject: Int): IntPreCondition
     fun that(subject: Double): DoublePreCondition
 }
+
+/**
+ * Implementation of [Assumptions] for tests.
+ */
+object TestAssumptions: Assumptions {
+    override fun that(subject: Int): IntPreCondition {
+        return IntPreCondition(subject)
+    }
+
+    override fun that(subject: Double): DoublePreCondition {
+        return DoublePreCondition(subject)
+    }
+}
+
+/**
+ * Statically available function to declare assumptions in tests.
+ */
 fun assume(): Assumptions {
     return TestAssumptions
 }
