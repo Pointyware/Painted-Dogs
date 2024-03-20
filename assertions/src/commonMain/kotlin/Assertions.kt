1fc9b581074d6731572102f8f8ef7f822686ca18
Taush Sampley <taushsampley@gmail.com>
2024-03-19 21:22:09 -0500

    Create Assertions interface to mirror Assumptions approach
    PreCondition classes will be modified to Condition classes for the sake of supporting traditional precondition, postcondition, and invariant expressions

--- assertions/src/commonMain/kotlin/Assertions.kt
+++ assertions/src/commonMain/kotlin/Assertions.kt
@@ -23,3 +23,41 @@ class Subject<T>(private val subject: T) {
 fun <T> assertThat(subject: T): Subject<T> {
     return Subject(subject)
 }
+
+/**
+ * Base class for declaring assertions to verify test results.
+ */
+interface Assertions {
+    fun that(subject: Int): IntPreCondition
+    fun that(subject: Double): DoublePreCondition
+    fun that(subject: String): StringPrecondition
+    fun <E> that(subject: Collection<E>): CollectionPrecondition<E>
+}
+
+/**
+ * Implementation of [Assertions] for tests.
+ */
+object TestAssertions: Assertions {
+    override fun that(subject: Int): IntPreCondition {
+        return IntPreCondition(subject)
+    }
+
+    override fun that(subject: Double): DoublePreCondition {
+        return DoublePreCondition(subject)
+    }
+
+    override fun that(subject: String): StringPrecondition {
+        return StringPrecondition(subject)
+    }
+
+    override fun <E> that(subject: Collection<E>): CollectionPrecondition<E> {
+        return CollectionPrecondition(subject)
+    }
+}
+
+/**
+ * Statically available function to declare assertions in tests.
+ */
+fun <T> assert(): Assertions {
+    return TestAssertions
+}
