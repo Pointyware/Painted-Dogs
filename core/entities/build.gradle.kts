19ce9b91d6a3b4b9bf0de9d3a44ef68c24a8862a
Taush Sampley <taushsampley@gmail.com>
2024-03-20 01:38:00 -0500

    Add :assertions to :core:entities

--- core/entities/build.gradle.kts
+++ core/entities/build.gradle.kts
@@ -42,6 +42,7 @@ kotlin {
         }
         val commonTest by getting {
             dependencies {
+                implementation(project(":assertions"))
                 implementation(libs.kotlin.test)
             }
         }
