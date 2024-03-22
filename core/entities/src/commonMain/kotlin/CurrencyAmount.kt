d6268505f2a75eb5b0898bf5eaa9f79293aa44f9
Taush Sampley <taushsampley@gmail.com>
2024-03-21 22:26:28 -0500

    Add currency unit to amount and update tests

--- core/entities/src/commonMain/kotlin/CurrencyAmount.kt
+++ core/entities/src/commonMain/kotlin/CurrencyAmount.kt
@@ -1,7 +1,19 @@
 package org.pointyware.painteddogs.core.entities
 
 data class CurrencyAmount constructor(
-    val amount: Double,
-) {
+    val amount: Long,
+    val unit: CurrencyUnit
+)
 
+enum class CurrencyUnit {
+    UsCents,
+    UsDollars,
+}
+
+fun Long.usCents(): CurrencyAmount {
+    return CurrencyAmount(this, CurrencyUnit.UsCents)
+}
+
+fun Long.usDollars(): CurrencyAmount {
+    return CurrencyAmount(this, CurrencyUnit.UsDollars)
 }
