2626706b29694e5dc7ded42eb90a41ab359a439e
Taush Sampley <taushsampley@gmail.com>
2024-03-23 10:26:19 -0500

    Implement  simple test view model

--- core/view-models/src/commonMain/kotlin/SimpleTestViewModel.kt
+++ core/view-models/src/commonMain/kotlin/SimpleTestViewModel.kt
@@ -1,5 +1,6 @@
 package org.pointyware.painteddogs.core.viewmodels
 
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 /**
@@ -26,4 +27,29 @@ interface SimpleTestViewModel<out S, out E> {
      * A complete list of all events that have been handled.
      */
     val eventLog: List<E>
+
+    infix fun on(event: @UnsafeVariance E) = handleEvent(event)
+}
+
+/**
+ * A simple implementation of [SimpleTestViewModel] that uses a [MutableStateFlow] to store state
+ */
+class SimpleTestViewModelImpl<out S, out E>(
+    initialState: S
+): SimpleTestViewModel<S, E> {
+    override fun setState(state: @UnsafeVariance S) {
+        _simpleState.value = state
+    }
+
+    private val _simpleState = MutableStateFlow<S>(initialState)
+    override val simpleState: StateFlow<S>
+        get() = _simpleState
+
+    override fun handleEvent(event: @UnsafeVariance E) {
+        _eventLog.add(event)
+    }
+
+    private val _eventLog = mutableListOf<E>()
+    override val eventLog: List<E>
+        get() = _eventLog
 }
