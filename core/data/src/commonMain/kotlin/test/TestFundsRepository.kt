600d088f259d7bf26e1a19235db707893003071f
Taush Sampley <taushsampley@gmail.com>
2024-03-23 09:19:06 -0500

    Swap string id with uuid in repo

--- core/data/src/commonMain/kotlin/test/TestFundsRepository.kt
+++ core/data/src/commonMain/kotlin/test/TestFundsRepository.kt
@@ -4,49 +4,43 @@ import kotlinx.coroutines.delay
 import org.pointyware.painteddogs.core.data.FundNotFoundException
 import org.pointyware.painteddogs.core.data.FundsRepository
 import org.pointyware.painteddogs.core.entities.Fund
+import org.pointyware.painteddogs.core.entities.Uuid
 
 /**
  * A simple implementation of [FundsRepository] that stores data in memory.
  */
 class TestFundsRepository(
     var defaultDelay: Long = 0
-): FundsRepository {
+): FundsRepository, BaseTestRepository {
 
-    private val funds = mutableMapOf<String, Fund>() // Simple in-memory storage
+    private val funds = mutableMapOf<Uuid, Fund>() // Simple in-memory storage
 
-    private suspend fun delayBefore() {
-        if (defaultDelay > 0) {
-            delay(defaultDelay)
-        }
-    }
-
-    override suspend fun getFund(collectionId: String): Result<Fund> {
-        delayBefore()
+    override suspend fun getFund(collectionId: Uuid): Result<Fund> {
         return funds[collectionId]?.let { Result.success(it) } ?: Result.failure(
             FundNotFoundException(collectionId)
         )
     }
 
+    suspend fun getFund(collectionId: Uuid, latency: Long = defaultDelay) = stubResponse(latency) {
+        getFund(collectionId)
+    }
+
     override suspend fun getFunds(): Result<List<Fund>> {
-        delayBefore()
         return Result.success(funds.values.toList())
     }
 
     override suspend fun addFund(collection: Fund) {
-        delayBefore()
         funds[collection.id] = collection
     }
 
     override suspend fun updateFund(collection: Fund): Result<Fund> {
-        delayBefore()
         return funds[collection.id]?.let {
             funds[collection.id] = collection
             Result.success(collection)
         } ?: Result.failure(FundNotFoundException(collection.id))
     }
 
-    override suspend fun deleteFund(collectionId: String) {
-        delayBefore()
+    override suspend fun deleteFund(collectionId: Uuid) {
         funds.remove(collectionId)
     }
 }
