66faaf73d61bcb12b7538b438d3f1301c9a63cff
Taush Sampley <taushsampley@gmail.com>
2024-03-22 20:43:27 -0500

    Add default delay to test repository methods

--- core/data/src/commonMain/kotlin/test/TestFundsRepository.kt
+++ core/data/src/commonMain/kotlin/test/TestFundsRepository.kt
@@ -1,5 +1,6 @@
 package org.pointyware.painteddogs.core.data.test
 
+import kotlinx.coroutines.delay
 import org.pointyware.painteddogs.core.data.FundNotFoundException
 import org.pointyware.painteddogs.core.data.FundsRepository
 import org.pointyware.painteddogs.core.entities.Fund
@@ -7,25 +8,37 @@ import org.pointyware.painteddogs.core.entities.Fund
 /**
  * A simple implementation of [FundsRepository] that stores data in memory.
  */
-class TestFundsRepository: FundsRepository {
+class TestFundsRepository(
+    var defaultDelay: Long = 0
+): FundsRepository {
 
     private val funds = mutableMapOf<String, Fund>() // Simple in-memory storage
 
+    private suspend fun delayBefore() {
+        if (defaultDelay > 0) {
+            delay(defaultDelay)
+        }
+    }
+
     override suspend fun getFund(collectionId: String): Result<Fund> {
+        delayBefore()
         return funds[collectionId]?.let { Result.success(it) } ?: Result.failure(
             FundNotFoundException(collectionId)
         )
     }
 
     override suspend fun getFunds(): Result<List<Fund>> {
+        delayBefore()
         return Result.success(funds.values.toList())
     }
 
     override suspend fun addFund(collection: Fund) {
+        delayBefore()
         funds[collection.id] = collection
     }
 
     override suspend fun updateFund(collection: Fund): Result<Fund> {
+        delayBefore()
         return funds[collection.id]?.let {
             funds[collection.id] = collection
             Result.success(collection)
@@ -33,6 +46,7 @@ class TestFundsRepository: FundsRepository {
     }
 
     override suspend fun deleteFund(collectionId: String) {
+        delayBefore()
         funds.remove(collectionId)
     }
 }
