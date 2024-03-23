32cee9fe7c4d1a09dc10721a52fecd38a7556ed3
Taush Sampley <taushsampley@gmail.com>
2024-03-23 09:39:02 -0500

    Create collection search view model and supporting ui states

---
+++ feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/viewmodels/CollectionSearchViewModel.kt
@@ -0,0 +1,18 @@
+package org.pointyware.painteddogs.feature.collections.core.viewmodels
+
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * ViewModel for searching donations/collections.
+ */
+interface CollectionSearchViewModel {
+    val state: StateFlow<CollectionSearchUiState>
+    /**
+     * Called when the search query changes.
+     */
+    fun onSearchQueryChanged(query: String)
+    /**
+     * Called when the search query is submitted.
+     */
+    fun onSubmitQuery(query: String)
+}
