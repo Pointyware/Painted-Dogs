0f450e752218befdc29ad0fc4725753dddb30d77
Taush Sampley <taushsampley@gmail.com>
2024-03-24 20:39:42 -0500

    cover missing guard clause

--- feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/interactors/CreateDonationUseCase.kt
+++ feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/interactors/CreateDonationUseCase.kt
@@ -4,6 +4,7 @@ import org.pointyware.painteddogs.core.entities.BlankStringArgumentException
 import org.pointyware.painteddogs.core.entities.CurrencyAmount
 import org.pointyware.painteddogs.core.entities.Fund
 import org.pointyware.painteddogs.core.entities.NumberArgumentException
+import org.pointyware.painteddogs.core.entities.StringArgumentException
 import org.pointyware.painteddogs.feature.collections.core.data.FundRepository
 import kotlin.coroutines.cancellation.CancellationException
 
@@ -20,14 +21,15 @@ class CreateDonationUseCaseImpl(
 ) : CreateDonationUseCase {
 
     private val exclusiveMinimum = 0
+    private val titleMaxLength = 100
 
     override suspend fun invoke(title: String, description: String, targetAmount: CurrencyAmount): Result<Fund> {
-        if (title.isBlank()) return Result.failure(
-            BlankStringArgumentException("Title"))
-        if (description.isBlank()) return Result.failure(
-            BlankStringArgumentException("Description"))
-        if (targetAmount.amount <= exclusiveMinimum) return Result.failure(
-            NumberArgumentException.AtMost(exclusiveMinimum, "Target Amount", targetAmount.amount))
+        when {
+            title.isBlank() -> return Result.failure(BlankStringArgumentException("Title"))
+            title.length >= titleMaxLength -> return Result.failure(StringArgumentException.LengthArgumentException.AtMost(titleMaxLength, "Title", title.length))
+            description.isBlank() -> return Result.failure(BlankStringArgumentException("Description"))
+            targetAmount.amount <= exclusiveMinimum -> return Result.failure(NumberArgumentException.AtMost(exclusiveMinimum, "Target Amount", targetAmount.amount))
+        }
 
         return repository.startDonationDrive(title, description, targetAmount)
     }
