2f3a85ccbbdb2a6a5b70804f7cc41f545e89379c
Taush Sampley <taushsampley@gmail.com>
2024-03-24 20:03:53 -0500

    Use standard exceptions to return descriptive results

--- feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/interactors/CreateDonationUseCase.kt
+++ feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/interactors/CreateDonationUseCase.kt
@@ -1,13 +1,17 @@
 package org.pointyware.painteddogs.feature.collections.core.interactors
 
+import org.pointyware.painteddogs.core.entities.BlankStringArgumentException
 import org.pointyware.painteddogs.core.entities.CurrencyAmount
 import org.pointyware.painteddogs.core.entities.Fund
+import org.pointyware.painteddogs.core.entities.NumberArgumentException
 import org.pointyware.painteddogs.feature.collections.core.data.FundRepository
+import kotlin.coroutines.cancellation.CancellationException
 
 interface CreateDonationUseCase {
     /**
      * @throws IllegalArgumentException if title or description are blank
      */
+    @Throws(BlankStringArgumentException::class, NumberArgumentException::class, CancellationException::class)
     suspend fun invoke(title: String, description: String, targetAmount: CurrencyAmount): Result<Fund>
 }
 
@@ -15,16 +19,15 @@ class CreateDonationUseCaseImpl(
     private val repository: FundRepository
 ) : CreateDonationUseCase {
 
-    /**
-     * @throws IllegalArgumentException if title or description are blank
-     */
+    private val exclusiveMinimum = 0
+
     override suspend fun invoke(title: String, description: String, targetAmount: CurrencyAmount): Result<Fund> {
-        if (title.isBlank() || description.isBlank()) {
-            return Result.failure(IllegalArgumentException("Title and description are required"))
-        }
-        if (targetAmount.amount <= 0) {
-            return Result.failure(IllegalArgumentException("Target amount must be greater than 0"))
-        }
+        if (title.isBlank()) return Result.failure(
+            BlankStringArgumentException("Title"))
+        if (description.isBlank()) return Result.failure(
+            BlankStringArgumentException("Description"))
+        if (targetAmount.amount <= exclusiveMinimum) return Result.failure(
+            NumberArgumentException.AtMost(exclusiveMinimum, "Target Amount", targetAmount.amount))
 
         return repository.startDonationDrive(title, description, targetAmount)
     }
