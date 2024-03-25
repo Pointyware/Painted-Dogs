703f7d1277492afcd67235e94e44c3ceb1961aaf
Taush Sampley <taushsampley@gmail.com>
2024-03-24 20:55:58 -0500

    Add more validation cases

--- feature/collections/core/src/commonTest/kotlin/org/pointyware/painteddogs/feature/collections/core/CreateDonationUseCaseTest.kt
+++ feature/collections/core/src/commonTest/kotlin/org/pointyware/painteddogs/feature/collections/core/CreateDonationUseCaseTest.kt
@@ -51,14 +51,60 @@ class CreateDonationUseCaseTest {
             targetAmount = 2000L.usDollars()
         )
 
-        /*
-        When the use case is invoked
-         */
         val result = service.invoke(given.title, given.description, given.targetAmount)
 
         assertTrue(result.isFailure)
         with(result.exceptionOrNull()!!) {
             assert().that(this::class).isEqualTo(StringArgumentException.LengthArgumentException.AtMost::class)
+        }
+    }
+
+    @Test
+    fun `description validation - too long`() = runTest {
+        val longDescription = generateString(1001) // Ensure a description exceeding the 1000-character limit
+        val given = DonationParams(
+            title = "Valid title",
+            description = longDescription,
+            targetAmount = 2000L.usDollars()
+        )
+
+        val result = service.invoke(given.title, given.description, given.targetAmount)
+
+        assertTrue(result.isFailure)
+        with(result.exceptionOrNull()!!) {
+            assert().that(this::class).isEqualTo(StringArgumentException.LengthArgumentException.AtMost::class)
+        }
+    }
+
+    @Test
+    fun `title validation - blank`() = runTest {
+        val given = DonationParams(
+            title = "",
+            description = "Valid description",
+            targetAmount = 2000L.usDollars()
+        )
+
+        val result = service.invoke(given.title, given.description, given.targetAmount)
+
+        assertTrue(result.isFailure)
+        with(result.exceptionOrNull()!!) {
+            assert().that(this::class).isEqualTo(StringArgumentException.BlankStringArgumentException::class)
+        }
+    }
+
+    @Test
+    fun `description validation - blank`() = runTest {
+        val given = DonationParams(
+            title = "Valid title",
+            description = "",
+            targetAmount = 2000L.usDollars()
+        )
+
+        val result = service.invoke(given.title, given.description, given.targetAmount)
+
+        assertTrue(result.isFailure)
+        with(result.exceptionOrNull()!!) {
+            assert().that(this::class).isEqualTo(StringArgumentException.BlankStringArgumentException::class)
         }
     }
 }
