f018ef30969716371cdbd845a01bd21b4db90f95
Taush Sampley <taushsampley@gmail.com>
2024-03-26 12:56:11 -0500

    Add date selector row and fund creation view

---
+++ feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/ui/DateSelectorRow.kt
@@ -0,0 +1,60 @@
+package org.pointyware.painteddogs.feature.collections.core.ui
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+data class DateSelectorRowState(
+    val isStartDateSelected: Boolean,
+    val title: String,
+    val datePickerState: DatePickerState,
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun rememberDateSelectorRowState(
+    isStartDateSelected: Boolean = false,
+    title: String = "",
+    datePickerState: DatePickerState = rememberDatePickerState(),
+) = remember(title) {
+    DateSelectorRowState(
+        isStartDateSelected = isStartDateSelected,
+        title = title,
+        datePickerState = datePickerState,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DateSelectorRow(
+    state: DateSelectorRowState = rememberDateSelectorRowState(),
+    modifier: Modifier = Modifier,
+    onStartDateSelectedChange: (Boolean) -> Unit,
+) {
+    Row(modifier = modifier.fillMaxWidth()) {
+        Checkbox(
+            checked = state.isStartDateSelected,
+            onCheckedChange = { onStartDateSelectedChange(it) },
+            modifier = Modifier.testTag("startDateCheckbox")
+        )
+        Text(
+            text = state.title,
+            modifier = Modifier.testTag("startDateText")
+        )
+        DatePicker(
+            state = state.datePickerState,
+            modifier = Modifier.testTag("datePicker")
+        )
+    }
+}
