b906aa2f150b4d26808baabf8de7b4cb43482f2e
Taush Sampley <taushsampley@gmail.com>
2024-03-27 13:05:56 -0500

    remove empty doc block

--- feature/collections/core/src/commonTest/kotlin/org/pointyware/painteddogs/feature/collections/core/ui/FundCreationViewUnitTest.kt
+++ feature/collections/core/src/commonTest/kotlin/org/pointyware/painteddogs/feature/collections/core/ui/FundCreationViewUnitTest.kt
@@ -6,20 +6,24 @@ import androidx.compose.ui.test.runComposeUiTest
 import org.pointyware.painteddogs.feature.collections.core.UiTest
 import kotlin.test.Test
 
-/**
- *
- */
 @OptIn(ExperimentalTestApi::class)
 @UiTest
 class FundCreationViewUnitTest {
     @Test
     fun `should display search input field`() = runComposeUiTest {
         // given - setup dependencies
+        val donationState = FundCreationViewState(
+            title = "Some title",
+            description = "Some description",
+            targetAmount = "$500",
+            startDate = "January 1",
+            endDate = "December 2",
+        )
 
         // when - the content is displayed
         setContent {
             FundCreationView(
-                state = rememberFundCreationViewState(),
+                state = donationState,
                 onTitleChange = {},
                 onDescriptionChange = {},
                 onGoalChange = {},
