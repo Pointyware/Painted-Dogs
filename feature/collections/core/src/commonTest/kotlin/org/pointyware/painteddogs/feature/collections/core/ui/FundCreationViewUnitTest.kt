6c5ed22200b3c91c18089db7be4600ce799d5977
Taush Sampley <taushsampley@gmail.com>
2024-03-28 07:28:29 -0500

    Move local variables to properties for mocking

--- feature/collections/core/src/commonTest/kotlin/org/pointyware/painteddogs/feature/collections/core/ui/FundCreationViewUnitTest.kt
+++ feature/collections/core/src/commonTest/kotlin/org/pointyware/painteddogs/feature/collections/core/ui/FundCreationViewUnitTest.kt
@@ -1,14 +1,16 @@
 package org.pointyware.painteddogs.feature.collections.core.ui
 
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.runComposeUiTest
 import io.mockative.Fun0
 import io.mockative.Fun1
+import io.mockative.Mock
 import io.mockative.classOf
 import io.mockative.mock
+import io.mockative.once
 import io.mockative.verify
 import org.pointyware.painteddogs.feature.collections.core.UiTest
 import kotlin.test.Test
@@ -16,15 +18,18 @@ import kotlin.test.Test
 @OptIn(ExperimentalTestApi::class)
 @UiTest
 class FundCreationViewUnitTest {
+    @Mock
+    val onEvent = mock(classOf<Fun0<Unit>>())
+    @Mock
+    val onStringEvent = mock(classOf<Fun1<String, Unit>>())
+    @Mock
+    val onBooleanEvent = mock(classOf<Fun1<Boolean, Unit>>())
+    @Mock
+    val onDoubleEvent = mock(classOf<Fun1<Double, Unit>>())
+
     @Test
     fun `should display search input field`() = runComposeUiTest {
-        // Test Setup
-        val onEvent = mock(classOf<Fun0<Unit>>())
-        val onStringEvent = mock(classOf<Fun1<String, Unit>>()) // Mock callback stub
-        val onBooleanEvent = mock(classOf<Fun1<Boolean, Unit>>())
-        val onDoubleEvent = mock(classOf<Fun1<Double, Unit>>())
-
-        // given - setup dependencies
+        // given some initial valid state
         val donationState = FundCreationViewState(
             title = "Some title",
             description = "Some description",
@@ -47,14 +52,34 @@ class FundCreationViewUnitTest {
         }
 
         // then - assert state
-        onNodeWithText("Search ", substring = true)
-        // Input Simulation (Using semantic properties)
-        onNode(hasText("Description")).performTextInput("Test Description")
+        onNodeWithText("Search ", substring = true).assertExists()
+    }
+
+    @Test
+    fun `search query when submit is tapped`() = runComposeUiTest {
+        // given some initial empty state
+
+        // when - the content is displayed
+        setContent {
+            FundCreationView(
+                state = rememberFundCreationViewState(),
+                onTitleChange = onStringEvent::invoke,
+                onDescriptionChange = onStringEvent::invoke,
+                onGoalChange = onDoubleEvent::invoke,
+                onStartDateSelectedChange = onBooleanEvent::invoke,
+                onEndDateSelectedChange = onBooleanEvent::invoke,
+                onSubmitDonation = onEvent::invoke,
+            )
+        }
         onNodeWithText("Title").performTextInput("Test Title")
+        onNodeWithText("Description").performTextInput("Test Description")
         onNodeWithText("Goal").performTextInput("1000.00")
+        onNodeWithText("Submit").performClick()
 
-        // Side-Effect Verification
-        verify { onStringEvent.invoke("Some title"); "" }
-
+        // Then methods are invoked with same arguments
+        verify { onStringEvent.invoke("Test Title") }.wasInvoked(once)
+        verify { onStringEvent.invoke("Test Description") }.wasInvoked(once)
+        verify { onDoubleEvent.invoke(1000.0) }.wasInvoked(once)
+        verify { onEvent.invoke() }.wasInvoked(once)
     }
 }
