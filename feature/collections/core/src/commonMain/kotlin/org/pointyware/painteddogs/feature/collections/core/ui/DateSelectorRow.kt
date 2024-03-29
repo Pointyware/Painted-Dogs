cb625e1fbeae2f72ff528b1e7b97a0100b0107ea
Taush Sampley <taushsampley@gmail.com>
2024-03-28 23:35:41 -0500

    Refine date selector; remove explicit optional nature in checkbox

--- feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/ui/DateSelectorRow.kt
+++ feature/collections/core/src/commonMain/kotlin/org/pointyware/painteddogs/feature/collections/core/ui/DateSelectorRow.kt
@@ -1,56 +1,77 @@
 package org.pointyware.painteddogs.feature.collections.core.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import kotlinx.datetime.Instant
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 data class DateSelectorRowState(
-    val isStartDateSelected: Boolean,
-    val title: String,
-    val datePickerState: DatePickerState,
+    val date: Instant?,
 )
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun rememberDateSelectorRowState(
-    isStartDateSelected: Boolean = false,
-    title: String = "",
-    datePickerState: DatePickerState = rememberDatePickerState(),
-) = remember(title) {
-    DateSelectorRowState(
-        isStartDateSelected = isStartDateSelected,
-        title = title,
-        datePickerState = datePickerState,
-    )
-}
-
+/**
+ * A row that allows a user to select or change a date or clear the selection.
+ * When a date is selected, the date is displayed in the row, with a button to clear the selection.
+ * The user can also click on the date to change it.
+ * When no date is selected, a button to select a date is displayed.
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DateSelectorRow(
-    state: DateSelectorRowState = rememberDateSelectorRowState(),
+    state: DateSelectorRowState,
     modifier: Modifier = Modifier,
-    onStartDateSelectedChange: (Boolean) -> Unit,
+    onDateChanged: (Instant?) -> Unit,
 ) {
     Row(modifier = modifier.fillMaxWidth()) {
-        Checkbox(
-            checked = state.isStartDateSelected,
-            onCheckedChange = { onStartDateSelectedChange(it) },
-        )
-        Text(
-            text = state.title,
-        )
-        DatePicker(
-            state = state.datePickerState,
-        )
+        var datePickerOpen by remember { mutableStateOf(false) }
+        // Date
+        state.date?.let { localDateTime ->
+            Text(
+                text = localDateTime.toString(),
+                modifier = Modifier.clickable {
+                    datePickerOpen = true
+                }
+            )
+        } ?: run {
+            Button(onClick = { datePickerOpen = true }) {
+                Text(text = "Select Date")
+            }
+        }
+
+        // Clear button
+        Button(onClick = { onDateChanged(null) }) {
+            Text(text = "Clear")
+        }
+
+        if (datePickerOpen) {
+            val pickerState = rememberDatePickerState(
+                initialSelectedDateMillis = state.date?.epochSeconds
+            )
+
+            DatePickerDialog(
+                onDismissRequest = { datePickerOpen = false },
+                dismissButton = { datePickerOpen = false },
+                confirmButton = {
+                    val selectedDateMillis = pickerState.selectedDateMillis ?: return@DatePickerDialog
+                    onDateChanged(Instant.fromEpochMilliseconds(selectedDateMillis))
+                }
+            ) {
+                DatePicker(
+                    state = pickerState,
+                )
+            }
+        }
     }
 }
