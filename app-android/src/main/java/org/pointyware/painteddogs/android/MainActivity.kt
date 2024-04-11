6b9abc954b5fe0ea8ba91a32e1ed4b986897e85c
Taush Sampley <taushsampley@gmail.com>
2024-04-11 08:55:29 -0500

    remove unused imports

--- app-android/src/main/java/org/pointyware/painteddogs/android/MainActivity.kt
+++ app-android/src/main/java/org/pointyware/painteddogs/android/MainActivity.kt
@@ -3,10 +3,8 @@ package org.pointyware.painteddogs.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
