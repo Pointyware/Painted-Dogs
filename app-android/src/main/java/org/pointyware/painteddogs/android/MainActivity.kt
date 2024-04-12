3cf9e6ce4c68afddb939b7b8540a4b45c5223df8
Taush Sampley <taushsampley@gmail.com>
2024-04-12 15:27:03 -0500

    Replace removed theme with app theme

--- app-android/src/main/java/org/pointyware/painteddogs/android/MainActivity.kt
+++ app-android/src/main/java/org/pointyware/painteddogs/android/MainActivity.kt
@@ -3,12 +3,16 @@ package org.pointyware.painteddogs.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import org.pointyware.painteddogs.core.ui.design.PaintedDogsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+            PaintedDogsTheme(
+                isDark = isSystemInDarkTheme()
+            ) {
 
             }
         }
