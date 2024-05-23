0a69571b1355b94ced6b38262eb5bcf449243cc8
Taush Sampley <taushsampley@gmail.com>
2024-05-22 22:49:55 -0500

    Move previews to :core:ui root package

--- core/ui/src/commonMain/kotlin/Previews.kt
+++ core/ui/src/commonMain/kotlin/Previews.kt
@@ -1,17 +1,16 @@
-package org.pointyware.painteddogs.core.ui.design
+package org.pointyware.painteddogs.core.ui
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import org.pointyware.painteddogs.core.ui.design.Dimension.Companion.by
+import org.pointyware.painteddogs.core.ui.Dimension.Companion.by
 
 enum class UiMode {
     Light,
