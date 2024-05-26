4b1a319765a747a0157646a7961f8651f3603b4c
Taush Sampley <taushsampley@gmail.com>
2024-05-26 11:48:48 -0500

    Mock profile view

--- core/ui/src/commonMain/kotlin/views/UserProfileView.kt
+++ core/ui/src/commonMain/kotlin/views/UserProfileView.kt
@@ -1,10 +1,17 @@
 package org.pointyware.painteddogs.core.ui.views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 data class UserProfileViewState(
     val userId: String,
+    val username: String,
+    val email: String,
+    val bio: String,
+    val profileImageUrl: String,
 )
 
 /**
@@ -12,7 +19,31 @@ data class UserProfileViewState(
  */
 @Composable
 fun UserProfileView(
+    state: UserProfileViewState,
     modifier: Modifier = Modifier,
 ) {
-
+    Column(
+        modifier = modifier,
+    ) {
+        Text(
+            text = "User Profile View",
+            style = MaterialTheme.typography.displaySmall,
+        )
+        Text(
+            text = "Username: ${state.username}",
+            style = MaterialTheme.typography.labelSmall
+        )
+        Text(
+            text = "Email: ${state.email}",
+            style = MaterialTheme.typography.labelSmall
+        )
+        Text(
+            text = "Bio: ${state.bio}",
+            style = MaterialTheme.typography.labelSmall
+        )
+        Text(
+            text = "Profile image URL: ${state.profileImageUrl}",
+            style = MaterialTheme.typography.labelSmall
+        )
+    }
 }
