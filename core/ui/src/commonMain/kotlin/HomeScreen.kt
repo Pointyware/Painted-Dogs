fbc93492f560f510221472a40893358f3bb41630
Taush Sampley <taushsampley@gmail.com>
2024-05-25 19:07:07 -0500

    Implement home screen

--- core/ui/src/commonMain/kotlin/HomeScreen.kt
+++ core/ui/src/commonMain/kotlin/HomeScreen.kt
@@ -1,19 +1,60 @@
 package org.pointyware.painteddogs.core.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import org.pointyware.painteddogs.core.ui.views.FundRow
+import org.pointyware.painteddogs.core.ui.views.FundRowState
+
+data class HomeScreenState(
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val funds: List<FundRowState> = emptyList(),
+)
 
 /**
  * Main view shown when there is no active navigation.
  */
 @Composable
 fun HomeScreen(
+    state: HomeScreenState,
     modifier: Modifier = Modifier,
+    onCreateFund: () -> Unit,
+    onSearchFunds: () -> Unit,
+    onViewProfile: () -> Unit,
+    onFundSelected: (String) -> Unit,
 ) {
-    /*
-    TODO: Implement home screen
-        1. Button => Create New Collection [x]
-        2. Button => Search Collections [x]
-        3. Button => View Profile [x]
-     */
+    Column(
+        modifier = modifier
+    ) {
+        LazyColumn(
+            modifier = Modifier.weight(1f)
+        ) {
+            items(state.funds) {
+                FundRow(
+                    state = it,
+                    onTap = { fundId -> onFundSelected(fundId) },
+                )
+            }
+        }
+        Button(
+            onClick = onCreateFund
+        ) {
+            Text(text = "Create New Collection")
+        }
+        Button(
+            onClick = onSearchFunds
+        ) {
+            Text(text = "Search Collections")
+        }
+        Button(
+            onClick = onViewProfile
+        ) {
+            Text(text = "View Profile")
+        }
+    }
 }
---
+++ core/ui/src/commonMain/kotlin/views/FundRowItem.kt
@@ -0,0 +1,43 @@
+package org.pointyware.painteddogs.core.ui.views
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+data class FundRowState(
+    val fundId: String,
+    val title: String,
+    val description: String,
+)
+
+/**
+ * Displays basic information about a fund in a row format.
+ */
+@Composable
+fun FundRow(
+    state: FundRowState,
+    modifier: Modifier = Modifier,
+    onTap: (String) -> Unit,
+) {
+    Row(
+        modifier = modifier
+            .padding(8.dp)
+            .clickable { onTap(state.fundId) }
+    ) {
+        Text(
+            text = state.title,
+            style = MaterialTheme.typography.titleSmall,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = state.description,
+            style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
