42e4bc77705ea0c656d0673499c999087b252e30
Taush Sampley <taushsampley@gmail.com>
2024-05-31 10:30:22 -0500

    Add docs and user info

--- core/analytics/src/commonMain/kotlin/AnalyticsInteractor.kt
+++ core/analytics/src/commonMain/kotlin/AnalyticsInteractor.kt
@@ -4,8 +4,21 @@ package org.pointyware.painteddogs.core.analytics
  * Interactor for logging analytics events.
  */
 interface AnalyticsInteractor {
+    /**
+     * Sets arbitrary information arguments about a user.
+     */
+    fun setUserInfo(info: Map<String, Any>)
+
+    /**
+     * Sets the default arguments that will be sent with each log event. Subsequent [logEvent] calls
+     * will include the given [defaults].
+     */
     fun setDefaultArguments(defaults: Map<String, Any>)
-    fun logEvent(eventName: String, eventParams: Map<String, Any>)
+
+    /**
+     * Logs an event with the given [name] and [arguments].
+     */
+    fun logEvent(eventName: String, arguments: Map<String, Any>)
 }
 
 /**
@@ -14,14 +27,19 @@ interface AnalyticsInteractor {
 class SimpleAnalyticsInteractor(
 ): AnalyticsInteractor {
 
-    private var defaults: Map<String, Any> = emptyMap()
+    private var userInfo: Map<String, Any> = emptyMap()
+    override fun setUserInfo(info: Map<String, Any>) {
+        userInfo = info
+        println("Set user information: $info")
+    }
 
+    private var defaults: Map<String, Any> = emptyMap()
     override fun setDefaultArguments(defaults: Map<String, Any>) {
         this.defaults = defaults
     }
 
-    override fun logEvent(eventName: String, eventParams: Map<String, Any>) {
-        val arguments = defaults + eventParams
-        println("Event: $eventName; Arguments: $arguments")
+    override fun logEvent(eventName: String, arguments: Map<String, Any>) {
+        val finalArgs = defaults + arguments
+        println("Event: $eventName; Arguments: $finalArgs")
     }
 }
