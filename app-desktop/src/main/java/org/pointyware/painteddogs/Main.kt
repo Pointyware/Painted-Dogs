25f797a72bee745581e3b0ff9942d63e6822afee
Taush Sampley <taushsampley@gmail.com>
2024-06-01 17:05:00 -0500

    remove unused imports

--- app-desktop/src/main/java/org/pointyware/painteddogs/Main.kt
+++ app-desktop/src/main/java/org/pointyware/painteddogs/Main.kt
@@ -1,13 +1,9 @@
 package org.pointyware.painteddogs
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import org.pointyware.painteddogs.app.PaintedDogsApp
-import org.pointyware.painteddogs.core.ui.design.PaintedDogsTheme
 
 fun main() = application {
 
