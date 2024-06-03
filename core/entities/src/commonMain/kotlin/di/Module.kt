af4646753135354ff1bf7db655cae54b83a8ba56
Taush Sampley <taushsampley@gmail.com>
2024-06-03 18:12:02 -0500

    Comment undefined bindings

--- core/entities/src/commonMain/kotlin/di/Module.kt
+++ core/entities/src/commonMain/kotlin/di/Module.kt
@@ -6,11 +6,11 @@ import org.koin.dsl.module
  * TODO: describe purpose/intent of Module
  */
 fun coreEntitiesModule() = module {
-    single { DogRepository() }
-    single { DogInteractor(get()) }
-    single { DogListInteractor(get()) }
-    single { DogDetailInteractor(get()) }
-    single { DogDetailViewModel(get()) }
-    single { DogListViewModel(get()) }
-    single { DogDetailViewModel(get()) }
+//    single { DogRepository() }
+//    single { DogInteractor(get()) }
+//    single { DogListInteractor(get()) }
+//    single { DogDetailInteractor(get()) }
+//    single { DogDetailViewModel(get()) }
+//    single { DogListViewModel(get()) }
+//    single { DogDetailViewModel(get()) }
 }
