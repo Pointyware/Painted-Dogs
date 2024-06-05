6a119773630fab13fea9fab7600a871fd68bf4d5
Taush Sampley <taushsampley@gmail.com>
2024-06-05 15:03:16 -0500

    remove todo comment

--- core/remote/src/commonMain/kotlin/di/Module.kt
+++ core/remote/src/commonMain/kotlin/di/Module.kt
@@ -3,7 +3,7 @@ package org.pointyware.painteddogs.core.remote.di
 import org.koin.dsl.module
 
 /**
- * TODO: describe purpose/intent of Module
+ *
  */
 fun coreRemoteModule() = module {
 //    single { RemoteInteractor(get()) }
