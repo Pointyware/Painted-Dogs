b8b207f8d09194c3a91f3ac2b93e35f93156c62f
Taush Sampley <taushsampley@gmail.com>
2024-06-08 16:25:48 -0500

    Add navigation dependencies to app container

--- app-shared/src/commonMain/kotlin/di/AppDependencies.kt
+++ app-shared/src/commonMain/kotlin/di/AppDependencies.kt
@@ -1,6 +1,8 @@
 package org.pointyware.painteddogs.app.di
 
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
+import org.pointyware.painteddogs.core.navigation.di.NavigationDependencies
 import org.pointyware.painteddogs.feature.collections.core.di.FundDependencies
 import org.pointyware.painteddogs.feature.payments.di.PaymentDependencies
 import org.pointyware.painteddogs.feature.profiles.di.ProfileDependencies
@@ -12,20 +14,15 @@ interface AppDependencies {
     fun provideProfileDependencies(): ProfileDependencies
     fun provideFundDependencies(): FundDependencies
     fun providePaymentDependencies(): PaymentDependencies
+
+    fun getNavigationDependencies(): NavigationDependencies
 }
 
 fun getDependencies(): AppDependencies = KoinAppDependencies()
 
 class KoinAppDependencies: AppDependencies, KoinComponent {
-    override fun provideProfileDependencies(): ProfileDependencies {
-        TODO("Not yet implemented")
-    }
-
-    override fun provideFundDependencies(): FundDependencies {
-        TODO("Not yet implemented")
-    }
-
-    override fun providePaymentDependencies(): PaymentDependencies {
-        TODO("Not yet implemented")
-    }
+    override fun provideProfileDependencies(): ProfileDependencies = get()
+    override fun provideFundDependencies(): FundDependencies = get()
+    override fun providePaymentDependencies(): PaymentDependencies = get()
+    override fun getNavigationDependencies(): NavigationDependencies = get()
 }
