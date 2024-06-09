fa7ecbb2f7b1b7990acee10cfe6d35680c6ce81e
Taush Sampley <taushsampley@gmail.com>
2024-06-08 21:20:05 -0500

    remember dependencies between compositions

--- app-android/src/main/java/org/pointyware/painteddogs/android/MainActivity.kt
+++ app-android/src/main/java/org/pointyware/painteddogs/android/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.remember
 import org.pointyware.painteddogs.app.PaintedDogsApp
 import org.pointyware.painteddogs.app.di.getDependencies
 
@@ -12,8 +13,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val appDependencies = remember { getDependencies() }
             PaintedDogsApp(
-                dependencies = getDependencies(),
+                dependencies = appDependencies,
                 isDarkTheme = isSystemInDarkTheme()
             )
         }
