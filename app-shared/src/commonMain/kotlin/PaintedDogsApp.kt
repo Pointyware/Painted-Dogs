3cbbb7650313bb08d48a1da512bf680de4c1cf0c
Taush Sampley <taushsampley@gmail.com>
2024-06-13 11:35:52 -0500

    Specify fab position

--- app-shared/src/commonMain/kotlin/PaintedDogsApp.kt
+++ app-shared/src/commonMain/kotlin/PaintedDogsApp.kt
@@ -2,6 +2,7 @@ package org.pointyware.painteddogs.shared
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
@@ -48,6 +49,7 @@ fun PaintedDogsApp(
                     Icon(painter = painterResource(Res.drawable.funds), contentDescription = "Create Fund")
                 }
             },
+            floatingActionButtonPosition = FabPosition.End,
             bottomBar = {
                 NavigationBar(
 //                    containerColor =
