298131e34d479c9df58ba35f4bbb342c263f271f
Taush Sampley <taushsampley@gmail.com>
2024-06-13 21:24:09 -0500

    remove unused buttons

--- app-shared/src/commonMain/kotlin/screens/HomeScreen.kt
+++ app-shared/src/commonMain/kotlin/screens/HomeScreen.kt
@@ -3,8 +3,6 @@ package org.pointyware.painteddogs.shared.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import org.pointyware.painteddogs.feature.funds.ui.FundRow
@@ -23,9 +21,6 @@ data class HomeScreenState(
 fun HomeScreen(
     state: HomeScreenState,
     modifier: Modifier = Modifier,
-    onCreateFund: () -> Unit,
-    onSearchFunds: () -> Unit,
-    onViewProfile: () -> Unit,
     onFundSelected: (String) -> Unit,
 ) {
     Column(
@@ -40,21 +35,6 @@ fun HomeScreen(
                     onTap = { fundId -> onFundSelected(fundId) },
                 )
             }
-        }
-        Button(
-            onClick = onCreateFund
-        ) {
-            Text(text = "Create New Collection")
-        }
-        Button(
-            onClick = onSearchFunds
-        ) {
-            Text(text = "Search Collections")
-        }
-        Button(
-            onClick = onViewProfile
-        ) {
-            Text(text = "View Profile")
         }
     }
 }
