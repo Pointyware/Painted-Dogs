63d2351bf669c5966afeebe754084f4f12dc673c
Taush Sampley <taushsampley@gmail.com>
2024-06-14 14:31:57 -0500

    Fix string instead of uuid

--- feature/profiles/src/commonMain/kotlin/viewmodels/ProfileUiState.kt
+++ feature/profiles/src/commonMain/kotlin/viewmodels/ProfileUiState.kt
@@ -1,10 +1,12 @@
 package org.pointyware.painteddogs.feature.profiles.viewmodels
 
+import org.pointyware.painteddogs.core.entities.Uuid
+
 /**
  *
  */
 data class ProfileUiState(
-    val id: String,
+    val id: Uuid,
     val username: String,
     val email: String,
     val avatarUrl: String,
