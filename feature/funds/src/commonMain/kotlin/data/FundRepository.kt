8966212dde73fa9786f6a29233f5e492b5b5c44d
Taush Sampley <taushsampley@gmail.com>
2024-06-15 15:16:10 -0500

    Add docs

--- feature/funds/src/commonMain/kotlin/data/FundRepository.kt
+++ feature/funds/src/commonMain/kotlin/data/FundRepository.kt
@@ -5,10 +5,31 @@ import org.pointyware.painteddogs.core.entities.Fund
 import org.pointyware.painteddogs.core.entities.Uuid
 
 /**
- *
+ * Defines the interface for a repository that manages funds.
  */
 interface FundRepository {
+    /**
+     * Creates a new fund with the given title, description, and target amount.
+     */
     suspend fun startDonationDrive(title: String, description: String, targetAmount: CurrencyAmount): Result<Fund>
+
+    /**
+     * Retrieves a fund by its ID, if it exists.
+     */
     suspend fun findById(id: Uuid): Fund?
+
+    /**
+     * Retrieves a list of funds that match the given query. A match is considered by title first then description.
+     */
     suspend fun search(query: String): Result<List<Fund>>
+
+    /**
+     * Updates the given fund with the new values. The result contains all allowed changes.
+     */
+    suspend fun update(fund: Fund): Result<Fund>
+
+    /**
+     * Deletes the given fund.
+     */
+    suspend fun delete(fund: Fund): Result<Unit>
 }
