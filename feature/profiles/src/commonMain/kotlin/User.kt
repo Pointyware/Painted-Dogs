10dc4fbcc1d0ec0fe45fac111d7fba0f7286c0a0
Taush Sampley <taushsampley@gmail.com>
2024-06-15 22:49:20 -0500

    Add user type to profiles module

---
+++ feature/profiles/src/commonMain/kotlin/User.kt
@@ -0,0 +1,13 @@
+package org.pointyware.painteddogs.feature.profiles
+
+import org.pointyware.painteddogs.core.entities.Uuid
+
+/**
+ *
+ */
+class User(
+    val id: Uuid,
+    val username: String
+) {
+
+}
