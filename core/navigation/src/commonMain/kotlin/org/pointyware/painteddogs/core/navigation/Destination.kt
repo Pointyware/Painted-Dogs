572ee1118d9cf69c2ec77263de83a720fa02dcde
Taush Sampley <taushsampley@gmail.com>
2025-08-17 03:32:35 -0500

    Remove serializable from interface

--- core/navigation/src/commonMain/kotlin/org/pointyware/painteddogs/core/navigation/Destination.kt
+++ core/navigation/src/commonMain/kotlin/org/pointyware/painteddogs/core/navigation/Destination.kt
@@ -1,11 +1,8 @@
 package org.pointyware.painteddogs.core.navigation
 
-import kotlinx.serialization.Serializable
-
 /**
  * All app destinations should inherit from this marker interface.
  */
-@Serializable
 interface Destination {
 
 }
