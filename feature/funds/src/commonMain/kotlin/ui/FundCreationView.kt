a1b75c6e18b651e9da66ef5b59000a28b3e25deb
Taush Sampley <taushsampley@gmail.com>
2025-08-16 22:18:57 -0500

    Opt-in to time api on FundCreationPreview

--- feature/funds/src/commonMain/kotlin/ui/FundCreationView.kt
+++ feature/funds/src/commonMain/kotlin/ui/FundCreationView.kt
@@ -14,11 +14,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import kotlin.time.ExperimentalTime
-import kotlin.time.Instant
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.pointyware.painteddogs.core.ui.components.DateSelectorRow
 import org.pointyware.painteddogs.core.ui.components.DateSelectorRowState
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 
 data class FundCreationViewState(
     val title: String,
@@ -137,6 +137,7 @@ fun FundCreationView(
     }
 }
 
+@OptIn(ExperimentalTime::class)
 @Preview
 @Composable
 fun FundCreationViewPreview() {
