5dd227b54258f51c3cc2773cfe3701f5d853a2c2
Taush Sampley <taushsampley@gmail.com>
2025-08-19 09:42:07 -0500

    Create message entity

---
+++ feature/chat/src/commonMain/kotlin/org/pointyware/painteddogs/chat/entities/Message.kt
@@ -0,0 +1,14 @@
+package org.pointyware.painteddogs.chat.entities
+
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
+
+/**
+ *
+ */
+@OptIn(ExperimentalTime::class)
+data class Message(
+    val participant: Participant,
+    val content: String,
+    val time: Instant
+)
