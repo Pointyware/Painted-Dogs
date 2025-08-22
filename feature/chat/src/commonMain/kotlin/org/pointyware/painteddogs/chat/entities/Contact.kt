3af04d787fa685eb9656bd89e3fffd6980b60e60
Taush Sampley <taushsampley@gmail.com>
2025-08-22 16:01:08 -0500

    Add Contact entity

---
+++ feature/chat/src/commonMain/kotlin/org/pointyware/painteddogs/chat/entities/Contact.kt
@@ -0,0 +1,9 @@
+package org.pointyware.painteddogs.chat.entities
+
+/**
+ * TODO: compare with Participant - participant seems like more like a Contact used in a specific context...
+ */
+data class Contact(
+    val id: String,
+    val username: String,
+)
