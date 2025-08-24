4e9ccb0198539751d954e6211cdfd76092b6d6df
Taush Sampley <taushsampley@gmail.com>
2025-08-24 08:05:15 -0500

    Implement feed logic

--- feature/chat/src/commonMain/kotlin/org/pointyware/painteddogs/chat/interactors/LoadMessagesUseCase.kt
+++ feature/chat/src/commonMain/kotlin/org/pointyware/painteddogs/chat/interactors/LoadMessagesUseCase.kt
@@ -1,25 +1,48 @@
 package org.pointyware.painteddogs.chat.interactors
 
-import org.pointyware.painteddogs.chat.data.ChatLog
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import org.pointyware.painteddogs.chat.data.AuthRepository
 import org.pointyware.painteddogs.chat.data.ChatRepository
-import org.pointyware.painteddogs.chat.data.ContactRepository
+import org.pointyware.painteddogs.chat.entities.Contact
+import kotlin.time.ExperimentalTime
 
+@OptIn(ExperimentalTime::class)
 class LoadMessagesUseCase(
     private val chatRepository: ChatRepository,
-    private val contactRepository: ContactRepository
+    private val authRepository: AuthRepository
 ) {
 
-    suspend fun invoke(chatId: String): Result<ChatLog> {
+    suspend fun invoke(chatId: String): Result<List<ChatLogItem>> {
         return chatRepository.getChat(chatId).mapCatching { header ->
-            val contacts = header.contactIds.map {
-                contactRepository.getContactById(it).getOrThrow()
+            val messages = chatRepository.getChatMessages(chatId).getOrThrow()
+
+            val currentUser = authRepository.currentUser
+            var lastDateTime: LocalDateTime? = null
+            var lastAuthor: Contact? = null
+
+            val elements = mutableListOf<ChatLogItem>()
+            messages.forEach { message ->
+                val newDateTime = message.timeSent.toLocalDateTime(TimeZone.currentSystemDefault())
+                val newAuthor = message.sender
+
+                if (newDateTime.year != lastDateTime?.year || newDateTime.month != lastDateTime.month || newDateTime.day != lastDateTime.day) {
+                    elements.add(ChatLogItem.TimeDivider(message.timeSent))
+                }
+                if (newAuthor != lastAuthor && newAuthor != currentUser) {
+                    elements.add(ChatLogItem.Author(message.sender))
+                }
+                elements.add(ChatLogItem.Message(
+                    content = message.content,
+                    timeStamp = message.timeSent,
+                    isSender = message.sender == currentUser
+                ))
+
+                lastDateTime = newDateTime
+                lastAuthor = newAuthor
             }
-            ChatLog(
-                id = header.id,
-                title = header.title,
-                participants = contacts,
-                messages = chatRepository.getChatMessages(chatId).getOrThrow(),
-            )
+            elements.toList()
         }
     }
 }
