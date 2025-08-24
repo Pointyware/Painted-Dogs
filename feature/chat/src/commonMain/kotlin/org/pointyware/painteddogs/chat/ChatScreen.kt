120e408d01b5e6949cc23274ba8a16e49875e616
Taush Sampley <taushsampley@gmail.com>
2025-08-24 05:55:35 -0500

    Move ChatViewScreen contents to dedicated composable

--- feature/chat/src/commonMain/kotlin/org/pointyware/painteddogs/chat/ChatScreen.kt
+++ feature/chat/src/commonMain/kotlin/org/pointyware/painteddogs/chat/ChatScreen.kt
@@ -1,22 +1,13 @@
 package org.pointyware.painteddogs.chat
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
-import org.pointyware.painteddogs.chat.ui.MessageBubble
-import org.pointyware.painteddogs.chat.ui.MessageBubbleState
-import org.pointyware.painteddogs.chat.viewmodels.ChatUiState
+import org.pointyware.painteddogs.chat.ui.ChatView
 import org.pointyware.painteddogs.chat.viewmodels.ChatViewModel
-import org.pointyware.painteddogs.core.ui.design.GeometryTokens
-import org.pointyware.painteddogs.core.ui.design.LocalGeometry
 
 @Composable
 fun ChatScreen(
@@ -26,39 +17,10 @@ fun ChatScreen(
     val chatMessages by viewModel.messages.collectAsState()
     val error by viewModel.error.collectAsState()
 
-    val errorCapture = error
-    if (errorCapture != null) {
-        Dialog(
-            onDismissRequest = { viewModel.onClearError() }
-        ) {
-            Text(
-                text = errorCapture.message ?: errorCapture.toString()
-            )
-        }
-    }
-    when (val capture = chatMessages) {
-        is ChatUiState.Loading -> {
-            Text("Loading")
-        }
-        is ChatUiState.UnknownChat -> {
-            Text("Unknown")
-        }
-        is ChatUiState.Loaded -> {
-            LazyColumn(
-                modifier = Modifier,
-                reverseLayout = true,
-                contentPadding = LocalGeometry.current.paddingSmall,
-                verticalArrangement = Arrangement.spacedBy(GeometryTokens.dpSmall),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                items(capture.messages) { message ->
-                    MessageBubble(
-                        MessageBubbleState(
-                            message.content
-                        )
-                    )
-                }
-            }
-        }
-    }
+    ChatView(
+        state = chatMessages,
+        error = error,
+        modifier = Modifier.fillMaxSize(),
+        onClearError = { viewModel.onClearError() }
+    )
 }
---
+++ feature/chat/src/commonMain/kotlin/org/pointyware/painteddogs/chat/ui/ChatView.kt
@@ -0,0 +1,57 @@
+package org.pointyware.painteddogs.chat.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Dialog
+import org.pointyware.painteddogs.chat.viewmodels.ChatUiState
+import org.pointyware.painteddogs.core.ui.design.GeometryTokens
+import org.pointyware.painteddogs.core.ui.design.LocalGeometry
+
+@Composable
+fun ChatView(
+    state: ChatUiState,
+    error: Throwable?,
+    modifier: Modifier = Modifier,
+    onClearError: ()->Unit,
+) {
+
+    if (error != null) {
+        Dialog(
+            onDismissRequest = onClearError
+        ) {
+            Text(
+                text = error.message ?: error.toString()
+            )
+        }
+    }
+    when (state) {
+        is ChatUiState.Loading -> {
+            Text("Loading")
+        }
+        is ChatUiState.UnknownChat -> {
+            Text("Unknown")
+        }
+        is ChatUiState.Loaded -> {
+            LazyColumn(
+                modifier = modifier,
+                reverseLayout = true,
+                contentPadding = LocalGeometry.current.paddingSmall,
+                verticalArrangement = Arrangement.spacedBy(GeometryTokens.dpSmall),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                items(state.messages) { message ->
+                    MessageBubble(
+                        MessageBubbleState(
+                            message.content
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
