b389aa50590ea73feb7c64e88e3f2db6577a14ea
Taush Sampley <taushsampley@gmail.com>
2025-08-26 10:06:33 -0500

    Add AidRouting documentation

--- feature/aid/src/commonMain/kotlin/org/pointyware/painteddogs/aid/navigation/AidDestination.kt
+++ feature/aid/src/commonMain/kotlin/org/pointyware/painteddogs/aid/navigation/AidDestination.kt
@@ -4,15 +4,27 @@ import kotlinx.serialization.Serializable
 import org.pointyware.painteddogs.aid.entities.Resource
 import org.pointyware.painteddogs.core.navigation.Destination
 
+/**
+ * Mutual Aid navigation destinations.
+ */
 sealed interface AidDestination: Destination {
+    /**
+     * The Mutual Aid notice board Screen with all requests and offers.
+     */
     @Serializable
     data object Board: AidDestination
 
+    /**
+     * The Screen where a user can offer a resource for the mutual aid network.
+     */
     @Serializable
     data class Offer(
         val resource: Resource
     ): AidDestination
 
+    /**
+     * The Screen where a user can request an offer for the mutual aid network.
+     */
     @Serializable
     data class Request(
         val resource: Resource
