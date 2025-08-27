0b4707340221c59dc6020e14d7a6a1b14cab6fa4
Taush Sampley <taushsampley@gmail.com>
2025-08-26 23:00:55 -0500

    Remove floating action button position

--- app-shared/src/commonMain/kotlin/org/pointyware/painteddogs/shared/PaintedDogsApp.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/painteddogs/shared/PaintedDogsApp.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -117,8 +116,7 @@ fun PaintedDogsApp(
                         )
                     }
                 }
-            },
-            floatingActionButtonPosition = FabPosition.End,
+            }
         ) { paddingValues ->
             NavHost(
                 navController = navController,
