f6fa255d9f1514d528f00f654a8e46a215795598
Taush Sampley <taushsampley@gmail.com>
2025-08-27 14:57:57 -0500

    Add request screen to aid routing

--- feature/aid/src/commonMain/kotlin/org/pointyware/painteddogs/aid/navigation/AidRouting.kt
+++ feature/aid/src/commonMain/kotlin/org/pointyware/painteddogs/aid/navigation/AidRouting.kt
@@ -1,10 +1,15 @@
 package org.pointyware.painteddogs.aid.navigation
 
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import org.pointyware.painteddogs.aid.MutualAidScreen
 import org.pointyware.painteddogs.aid.OfferScreen
+import org.pointyware.painteddogs.aid.ui.RequestScreen
+import org.pointyware.painteddogs.aid.ui.RequestScreenState
+import org.pointyware.painteddogs.aid.viewmodels.RequestViewModel
 import org.pointyware.painteddogs.core.ui.composeKoinViewModel
 
 /**
@@ -28,6 +33,17 @@ fun NavGraphBuilder.aidRouting(
     }
 
     composable<AidDestination.Request> {
-
+        val viewModel: RequestViewModel = composeKoinViewModel()
+        val state by viewModel.state.collectAsState()
+        RequestScreen(
+            state = state.let {
+                RequestScreenState(
+                    temporalScope = it.temporalScope,
+                    description = it.description,
+                )
+            },
+            onScopeSelected = viewModel::onTemporalScopeSelected,
+            onSubmit = viewModel::onSubmit,
+        )
     }
 }
