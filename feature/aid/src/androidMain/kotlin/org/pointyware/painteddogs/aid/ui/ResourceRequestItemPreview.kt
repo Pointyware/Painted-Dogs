1ed2ad9955ee558b94ce3588797dbc611a926e82
Taush Sampley <taushsampley@gmail.com>
2025-08-31 05:34:14 -0500

    Move ResourceRequestItemStateProvider to dedicated file

--- feature/aid/src/androidMain/kotlin/org/pointyware/painteddogs/aid/ui/ResourceRequestItemPreview.kt
+++ feature/aid/src/androidMain/kotlin/org/pointyware/painteddogs/aid/ui/ResourceRequestItemPreview.kt
@@ -3,12 +3,7 @@ package org.pointyware.painteddogs.aid.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import org.pointyware.painteddogs.aid.entities.Resource
-import org.pointyware.painteddogs.aid.entities.ResourceRequest
 import org.pointyware.painteddogs.core.ui.design.PaintedDogsTheme
-import kotlin.time.Clock
-import kotlin.time.Duration.Companion.hours
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
@@ -24,47 +19,4 @@ private fun ResourceRequestItemPreview(
             onProvideResource = { },
         )
     }
-}
-
-
-@OptIn(ExperimentalTime::class)
-class ResourceRequestItemStateProvider: PreviewParameterProvider<ResourceRequestItemState> {
-    override val values: Sequence<ResourceRequestItemState>
-        get() = sequenceOf(
-            ResourceRequestItemState(
-                request = ResourceRequest(
-                    category = Resource.Food,
-                    description = "Some food item",
-                    timePosted = Clock.System.now() - 1.hours
-                )
-            ),
-            ResourceRequestItemState(
-                request = ResourceRequest(
-                    category = Resource.Housing,
-                    description = "3 Beds",
-                    timePosted = Clock.System.now() - 1.5.hours
-                )
-            ),
-            ResourceRequestItemState(
-                request = ResourceRequest(
-                    category = Resource.Funds,
-                    description = "50 for New Tire",
-                    timePosted = Clock.System.now() - 5.7.hours
-                )
-            ),
-            ResourceRequestItemState(
-                request = ResourceRequest(
-                    category = Resource.Skills,
-                    description = "Budgeting",
-                    timePosted = Clock.System.now() - 36.hours
-                )
-            ),
-            ResourceRequestItemState(
-                request = ResourceRequest(
-                    category = Resource.Protection,
-                    description = "Escort to School",
-                    timePosted = Clock.System.now() - 300.3.hours
-                )
-            )
-        )
 }
---
+++ feature/aid/src/androidMain/kotlin/org/pointyware/painteddogs/aid/ui/ResourceRequestItemStateProvider.kt
@@ -0,0 +1,60 @@
+package org.pointyware.painteddogs.aid.ui
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import org.pointyware.painteddogs.aid.entities.Resource
+import org.pointyware.painteddogs.aid.entities.ResourceRequest
+import org.pointyware.painteddogs.aid.entities.TemporalScope
+import kotlin.time.Clock
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.ExperimentalTime
+
+
+@OptIn(ExperimentalTime::class)
+private val resourceRequestItemStateSequence = sequenceOf(
+    ResourceRequestItemState(
+        request = ResourceRequest(
+            category = Resource.Food,
+            scope = TemporalScope.Indefinite,
+            description = "Some food item",
+            timePosted = Clock.System.now() - 1.hours
+        )
+    ),
+    ResourceRequestItemState(
+        request = ResourceRequest(
+            category = Resource.Housing,
+            scope = TemporalScope.Indefinite,
+            description = "3 Beds",
+            timePosted = Clock.System.now() - 1.5.hours
+        )
+    ),
+    ResourceRequestItemState(
+        request = ResourceRequest(
+            category = Resource.Funds,
+            scope = TemporalScope.Indefinite,
+            description = "50 for New Tire",
+            timePosted = Clock.System.now() - 5.7.hours
+        )
+    ),
+    ResourceRequestItemState(
+        request = ResourceRequest(
+            category = Resource.Skills,
+            scope = TemporalScope.Indefinite,
+            description = "Budgeting",
+            timePosted = Clock.System.now() - 36.hours
+        )
+    ),
+    ResourceRequestItemState(
+        request = ResourceRequest(
+            category = Resource.Protection,
+            scope = TemporalScope.Indefinite,
+            description = "Escort to School",
+            timePosted = Clock.System.now() - 300.3.hours
+        )
+    )
+)
+
+@OptIn(ExperimentalTime::class)
+class ResourceRequestItemStateProvider: PreviewParameterProvider<ResourceRequestItemState> {
+    override val values: Sequence<ResourceRequestItemState>
+        get() = resourceRequestItemStateSequence
+}
