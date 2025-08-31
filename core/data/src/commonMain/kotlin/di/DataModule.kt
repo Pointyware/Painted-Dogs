4fe861c031669725caab76629a4493751a8ddae6
Taush Sampley <taushsampley@gmail.com>
2025-08-31 09:53:55 -0500

    Remove data dependencies

--- core/data/src/commonMain/kotlin/di/DataModule.kt
+++ core/data/src/commonMain/kotlin/di/DataModule.kt
@@ -2,7 +2,6 @@ package org.pointyware.painteddogs.core.data.di
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
 import kotlinx.coroutines.SupervisorJob
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
@@ -15,7 +14,6 @@ val dataQualifier = named("data-scope")
  * Collects all individual :core:data dependency bindings into a single module.
  */
 fun dataModule() = module {
-    single<DataDependencies> { KoinDataDependencies() }
     single<CoroutineScope>(qualifier = dataQualifier) { CoroutineScope(Dispatchers.IO + SupervisorJob()) }
     includes(
         repositoryModule()
