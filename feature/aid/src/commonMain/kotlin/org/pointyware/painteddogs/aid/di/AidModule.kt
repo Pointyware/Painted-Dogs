9cc900770c30497fcbd19c6c3f3705ba31db096e
Taush Sampley <taushsampley@gmail.com>
2025-08-31 21:14:11 -0500

    Supply missing interactors in koin module

--- feature/aid/src/commonMain/kotlin/org/pointyware/painteddogs/aid/di/AidModule.kt
+++ feature/aid/src/commonMain/kotlin/org/pointyware/painteddogs/aid/di/AidModule.kt
@@ -5,6 +5,8 @@ import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 import org.pointyware.painteddogs.aid.data.AidRepository
 import org.pointyware.painteddogs.aid.data.fake.FakeAidRepository
+import org.pointyware.painteddogs.aid.interactors.CreateOfferUseCase
+import org.pointyware.painteddogs.aid.interactors.CreateRequestUseCase
 import org.pointyware.painteddogs.aid.viewmodels.MutualAidViewModel
 import org.pointyware.painteddogs.aid.viewmodels.OfferViewModel
 import org.pointyware.painteddogs.core.data.di.dataQualifier
@@ -13,7 +15,8 @@ import kotlin.coroutines.CoroutineContext
 fun aidModule() = module {
     factoryOf(::MutualAidViewModel)
     factoryOf(::OfferViewModel)
-//    factoryOf()
+    factoryOf(::CreateOfferUseCase)
+    factoryOf(::CreateRequestUseCase)
 
     single<AidRepository> {
         FakeAidRepository(
