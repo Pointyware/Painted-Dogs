46e222fb1b6e999e21a1b5a42a5cc18d5c138749
Taush Sampley <taushsampley@gmail.com>
2025-09-02 17:11:14 -0500

    Add contact image composable

---
+++ feature/chat/src/commonMain/kotlin/org/pointyware/painteddogs/chat/ui/ContactImage.kt
@@ -0,0 +1,28 @@
+package org.pointyware.painteddogs.chat.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
+import org.pointyware.painteddogs.chat.Res
+import org.pointyware.painteddogs.chat.acc_no_profile_picture
+import org.pointyware.painteddogs.chat.entities.Contact
+import org.pointyware.painteddogs.chat.outline_person_24
+import org.pointyware.painteddogs.core.ui.outline_question_mark_24
+import org.pointyware.painteddogs.core.ui.Res as UiRes
+
+@Composable
+fun ContactImage(
+    value: Contact?,
+    modifier: Modifier = Modifier
+) {
+    val image = value?.let {
+        vectorResource(UiRes.drawable.outline_question_mark_24)
+    } ?: vectorResource(Res.drawable.outline_person_24)
+    Image(
+        modifier = modifier,
+        imageVector = image,
+        contentDescription = value?.username ?: stringResource(Res.string.acc_no_profile_picture)
+    )
+}
