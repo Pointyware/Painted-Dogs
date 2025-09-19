7f5566596e12a86601b6b35aaa8441279a7cbbd9
Taush Sampley <taushsampley@gmail.com>
2025-09-19 00:36:50 -0500

    Move quantity entity to existing package

--- core/entities/src/commonMain/kotlin/org/pointyware/painteddogs/entities/Quantity.kt
+++ core/entities/src/commonMain/kotlin/org/pointyware/painteddogs/entities/Quantity.kt
@@ -3,6 +3,91 @@ package org.pointyware.painteddogs.entities
 /**
  *
  */
-sealed interface Quantity {
+sealed class Measurement {
 
+    /**
+     * The base [Measurement] used for conversions.
+     */
+    abstract val commonBase: Measurement
+
+    /**
+     * The number of the given units per measurement base.
+     * This is exactly 1 for [commonBase]#[unitsPerBase].
+     */
+    abstract val unitsPerBase: Double
+    fun toCommon(value: Double): Double {
+        return value / unitsPerBase
+    }
+
+    /**
+     *
+     */
+    abstract class Length(
+        val unitsPerMeter: Double
+    ): Measurement() {
+        override val commonBase: Measurement
+            get() = Meters
+        override val unitsPerBase: Double
+            get() = unitsPerMeter
+
+        data object Centimeters: Length(1e1)
+        data object Meters: Length(1e0)
+        data object Kilometers: Length(1e-3)
+        data object Feet: Length(3.28084)
+        data object Inches: Length(39.3701)
+    }
+
+    /**
+     *
+     */
+    abstract class Mass(
+        val unitsPerGram: Double
+    ): Measurement() {
+        override val commonBase: Measurement
+            get() = Grams
+        override val unitsPerBase: Double
+            get() = unitsPerGram
+
+        data object Grams: Mass(1e0)
+        data object Kilograms: Mass(1e-3)
+        data object Pounds: Mass(.00220462)
+    }
+
+    /**
+     *
+     */
+    abstract class Force(
+        val unitsPerNewton: Double
+    ): Measurement() {
+        override val commonBase: Measurement
+            get() = Newtons
+        override val unitsPerBase: Double
+            get() = unitsPerNewton
+
+        data object Newtons: Force(1e0)
+        data object Pounds: Force(0.224809)
+    }
+}
+
+/**
+ *
+ */
+sealed interface Quantity<M:Measurement> {
+    val value: Double
+    val measurement: M
+}
+
+/**
+ *
+ */
+private data class SimpleQuantity<M:Measurement>(
+    override val value: Double,
+    override val measurement: M
+): Quantity<M>
+
+fun Double.meters(): Quantity<Measurement.Length> {
+    return SimpleQuantity(
+        this,
+        Measurement.Length.Meters
+    )
 }
--- feature/aid/build.gradle.kts
+++ feature/aid/build.gradle.kts
@@ -21,6 +21,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(projects.core.common)
+                implementation(projects.core.entities)
                 implementation(projects.core.data)
                 implementation(projects.core.navigation)
                 implementation(projects.core.viewModels)
