466e705d8d71b2ccd2a127524a6d1b93295f776e
Taush Sampley <taushsampley@gmail.com>
2025-10-15 20:33:09 -0500

    expose errors in support request view model

--- feature/aid/src/commonMain/kotlin/org/pointyware/painteddogs/aid/viewmodels/SupportRequestViewModel.kt
+++ feature/aid/src/commonMain/kotlin/org/pointyware/painteddogs/aid/viewmodels/SupportRequestViewModel.kt
@@ -5,8 +5,12 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import org.pointyware.painteddogs.aid.entities.ResourceRequest
 import org.pointyware.painteddogs.aid.interactors.LoadResourceRequestUseCase
+import org.pointyware.painteddogs.entities.Quantity
+import org.pointyware.painteddogs.entities.RealUnit
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -21,16 +25,27 @@ class SupportRequestViewModel(
     private val mutableState = MutableStateFlow<Any?>(null)
     val state: StateFlow<Any?> get() = mutableState.asStateFlow()
 
+    private val mutableError = MutableStateFlow<Throwable?>(null)
+    val error: StateFlow<Throwable?> = mutableError.asStateFlow()
+
     fun loadRequest(requestId: String) {
         viewModelScope.launch {
             loadRequestUseCase.invoke(Uuid.parse(requestId))
                 .onSuccess { request ->
-
+                    mutableState.update {
+                        // TODO: bind resulting state
+                    }
                 }
                 .onFailure { throwable ->
-
+                    mutableError.value = throwable
                 }
         }
     }
 
 }
+
+data class SupportRequestUiState(
+    val request: ResourceRequest,
+
+    val quantity: Quantity<RealUnit>,
+)
