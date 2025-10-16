78f1a14f7ba2eda5a2e62e73d47960b41117f56b
Taush Sampley <taushsampley@gmail.com>
2025-10-16 04:13:16 -0500

    Document callback purposes

--- feature/aid/src/commonMain/kotlin/org/pointyware/painteddogs/aid/navigation/AidRouting.kt
+++ feature/aid/src/commonMain/kotlin/org/pointyware/painteddogs/aid/navigation/AidRouting.kt
@@ -7,17 +7,24 @@ import kotlin.uuid.Uuid
 
 /**
  * Routing logic for Mutual Aid screens.
+ *
+ * @param navigateToRequestDetail Navigate to a detail screen where a user can
+ * confirm request details.
+ * @param onClaimOffer Navigate to claim confirmation screen.
+ * @param onNavigateToRequest Navigate to specific resource request info screen.
+ * @param onNavigateToOffer Navigate to specific resource offer info screen.
+ * @param onCreateOffer Navigate to a detail screen where a user can confirm offer details.
  */
 @OptIn(ExperimentalUuidApi::class)
 fun NavGraphBuilder.aidRouting(
-    onSupportRequest: (Uuid)->Unit,
+    navigateToRequestDetail: (Uuid)->Unit,
     onClaimOffer: (Uuid)->Unit,
     onNavigateToRequest: (Uuid)->Unit,
     onNavigateToOffer: (Uuid)-> Unit,
     onCreateOffer: (Resource)->Unit,
 ) {
     exchangeBoardDestination(
-        onSupportRequest = onSupportRequest,
+        onSupportRequest = navigateToRequestDetail,
         onClaimOffer = onClaimOffer,
         onCreateOffer = onCreateOffer
     )
