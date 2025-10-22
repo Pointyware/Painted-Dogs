758ba1e8393a89aeffd69fa5c39b1c11e06b7985
Taush Sampley <taushsampley@gmail.com>
2025-10-21 20:58:09 -0500

    Create dedicated Volume real unit file

---
+++ core/entities/src/commonMain/kotlin/org/pointyware/painteddogs/entities/Volume.kt
@@ -0,0 +1,20 @@
+package org.pointyware.painteddogs.entities
+
+abstract class Volume(
+    val unitsPerLiter: Double
+): RealUnit() {
+    override val commonBase: RealUnit
+        get() = Liters
+    override val unitsPerBase: Double
+        get() = unitsPerLiter
+}
+
+object Liters: Volume(1e0)
+object Milliliters: Volume(1e3)
+
+object Teaspoon: Volume(202.884)
+object Tablespoon: Volume(67.628)
+object Cup: Volume(4.22675)
+object Pint: Volume(2.11338)
+object Quart: Volume(1.05669)
+object Gallon: Volume(0.264172)
